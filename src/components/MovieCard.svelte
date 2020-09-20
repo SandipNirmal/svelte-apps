5b35e0fe2b37e8f256d119961261bdf29337c56f
Sandip Nirmal <sandipnirmal@icloud.com>
2020-09-20 15:46:55 +0530

    Basic Card Layout

--- src/components/MovieCard.svelte
+++ src/components/MovieCard.svelte
@@ -1,15 +1,48 @@
 <script>
   export let title = '';
-  export let description = '';
+  // export let description = '';
   export let image = '';
   export let media_type = 'movie';
   export let ratings = 5;
 </script>
 
-<div
-  class="w-32 h-48 bg-gray-600 relative bg-cover"
-  style="background-image: url({image})">
-  <div class="absolute p-1 bg-gray-600 bottom-0">
-    <h5 class="text-sm font-medium">{title}</h5>
+<style>
+  div.card-wrapper {
+    width: 140px;
+    max-width: 140px;
+  }
+
+  .card {
+    transition: all 0.3s;
+  }
+
+  .card:hover,
+  .card:focus {
+    transform: scale(1.1);
+    cursor: pointer;
+    box-shadow: 0 20px 25px 5px rgba(0, 0, 0, 0.1),
+      0 10px 10px 5px rgba(0, 0, 0, 0.04);
+  }
+
+  .media_type {
+    font-size: 10px;
+    padding: 1px 2px;
+  }
+</style>
+
+<div class="card-wrapper flex flex-col items-center m-2">
+  <div
+    class="card w-32 h-48 bg-gray-700 bg-cover shadow-md"
+    style="background-image: url({image})" />
+
+  <div class="text-xs w-full">
+    <div class="pt-4 px-1">
+      <h5 class="font-medium">{title}</h5>
+    </div>
+    <div class="flex justify-between px-1 py-1">
+      <span
+        class="media_type bg-teal-300 uppercase rounded-sm">{media_type}</span>
+      <span class="font-medium text-gray-500">{ratings}/10</span>
+    </div>
   </div>
 </div>
---
+++ src/routes/movies-app/index.svelte
@@ -0,0 +1,31 @@
+<script>
+  import Nav from './_nav.svelte';
+  import Card from '../../components/MovieCard.svelte';
+
+  import { trending } from './data';
+</script>
+
+<!--<style>
+  .movie-app {
+    background-color: #f8f8f8;
+  }
+</style>-->
+
+<div class="movie-app w-screen h-screen">
+  <Nav />
+
+  <article
+    class="p-4 my-8 w-full flex flex-wrap justify-center sm:justify-start">
+    {#each trending as { title, name, vote_average, poster_path, media_type }}
+      <Card
+        title={title || name}
+        ratings={vote_average}
+        {media_type}
+        image="https://image.tmdb.org/t/p/w500/{poster_path}" />
+    {/each}
+
+    <!-- <div>Carousal</div>
+    <div>Tabs</div>
+    <div>Content</div> -->
+  </article>
+</div>
